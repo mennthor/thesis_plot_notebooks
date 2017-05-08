a5a0c342f72377299df57a6121b510983911fe0d
Thorben Menne <thorben.menne@tu-dortmund.de>
2017-05-08 19:29:58 -0400

    Rewrote bg_injector for use with adaptive bs KDE. Cleared up code and used replaced emcee smapling with inversion method from pythia8. Updates in docs. (Fake) Bounds can be used in KDE.

--- bg_injector.py
+++ bg_injector.py
@@ -6,8 +6,9 @@ given event position from experimental data.
 """
 
 import numpy as np
-import sklearn.neighbors as skn
 from sklearn.utils import check_random_state
+
+import anapymods3.stats.KDE as KDE
 
 
 class BGInjector(object):
@@ -23,7 +24,7 @@ class BGInjector(object):
         print("Interface only. Defines functions: ", self._IMPLEMENTS)
         return
 
-    def fit(self, X):
+    def fit(self, X, bounds=None):
         """
         Build the injection model with the provided data
 
@@ -32,22 +33,22 @@ class BGInjector(object):
         X : array_like, shape (n_samples, n_features)
             List of n_features-dimensional data points.  Each row
             corresponds to a single data point.
+        bounds : array-like, shape (n_features, 2)
+            Boundary conditions for each dimension. (default: None)
         """
         raise NotImplementedError("BGInjector is an interface.")
 
-    def sample(self, n_samples=1, range=None, random_state=None):
+    def sample(self, n_samples=1, random_state=None):
         """
         Generate random samples from the fitted model.
 
         Parameters
         ----------
         n_samples : int, optional
-            Number of samples to generate. (defaults: 1)
-        range : array-like, shape (2, n_features)
-            Give a range [low, hig] in which events shall be sampled.
-            (default: None)
+            Number of samples to generate. (default: 1)
         random_state : RandomState, optional
-            A random number generator instance. (default: None)
+            Turn seed into a `np.random.RandomState` instance. Method from
+            `sklearn.utils`. Can be None, int or RndState. (default: None)
 
         Returns
         -------
@@ -56,97 +57,100 @@ class BGInjector(object):
         """
         raise NotImplementedError("BGInjector is an interface.")
 
+    # Private methods
+    def _check_bounds(self, bounds):
+        """
+        Check if bounds are OK. Create numerical values when None is given.
 
-class FixedBandwidthKDEBGInjector(BGInjector):
+        Returns
+        -------
+        bounds : array-like, shape (n_features, 2)
+            Boundary conditions for each dimension.
+        """
+        if bounds is None:
+            bounds = np.repeat([[-np.inf, np.inf], ],
+                               repeats=self._n_features, axis=0)
+
+        bounds = np.array(bounds)
+        if bounds.shape[1] != 2:
+            raise ValueError("Invalid 'bounds'. Must be shape (n_features, 2).")
+
+        # Convert None to +-np.inf depnding on low/hig bound
+        bounds[:, 0][bounds[:, 0] == np.array(None)] = -np.inf
+        bounds[:, 1][bounds[:, 1] == np.array(None)] = +np.inf
+
+        return bounds
+
+    def _check_sample_params(self, n_samples, random_state):
+        """
+        Check if bounds are OK.
+
+        Returns
+        -------
+        random_state : np.random.RandomState
+            Random state instance created by `sklearn.utils.check_random_state`.
+        """
+        if n_samples < 1:
+            raise ValueError("'n_samples' must be at least 1.")
+
+        return check_random_state(random_state)
+
+
+class KDEBGInjector(BGInjector):
     """
-    Fixed Bandwidth Kernel Density Background Injector
+    Adaptive Bandwidth Kernel Density Background Injector.
 
-    Background Injector using a kernel density estimator with a fixed bandwidth
-    to describe the background pdf.
-    This is a wrapper for `sklearn.neighbours.KernelDensity` which implements
-    a `fit` and a `sample`method.
-    The sample method is only supported for 'gaussian' and 'tophat' kernels.
+    Parameters are passed to the KDE class. Fitting of the model can take some
+    time (60min / 100k evts) when adaptive kernels are used.
 
     Parameters
     ----------
-    XtoY : function, call signature XtoY(X), returns Y
-        Function, that takes the data X in and transforms the to be suitable for
-        a global KDE. Returns Y with same shape but transformed values. The KDE
-        is build on Y afterwards.
-    YtoX : function, call signature YtoX(Y), returns X
-        Backtransformation of `XtoY`. Returns X in the original paramter
-        space after sampling.
-    bandwidth : float
-        The bandwidth of the kernel. Should be optimized in a seperate step in
-        the same space as XtoY implies. (default: 1.)
-    kernel : string
-        The kernel to use. Valid kernels are ['gaussian'|'tophat'], the only
-        two that have the sample method. (default: 'gaussian')
-    rtol : float
-        The desired relative tolerance of the result.  A larger tolerance will
-        generally lead to faster execution. (default: 1e-8)
-
-    KDEargs
-    -------
-    Other parameters are passed directly to the the
-    `sklearn.neighbours.KernelDensity` class.
+    glob_bw : float or str
+        The global bandwidth of the kernel, must be a float > 0 or one of
+        ["silverman"|"scott"]. If alpha is not None, this is the bandwidth for
+        the first estimate KDE from which the local bandwidth is calculated.
+        If ["silverman"|"scott"] a rule of thumb is used to estimate the
+        bandwidth. (default: "silverman")
+    alpha : float or None
+        If None, only the global bandwidth is used. If 0 <= alpha <= 1, an
+        adaptive local kernel bandwith is used as described in. (default: 0.5)
+    diag_cov : bool
+        If True, only scale by variance, diagonal cov matrix. (default: False)
+    max_gb : float
+        Maximum gigabyte of RAM occupied in evaluating the KDE.
     """
-    _VALID_KERNELS = ["gaussian", "tophat"]
-
-    def __init__(self, XtoY, YtoX, bandwidth=1.,
-                 kernel="gaussian", rtol=1e-8, **KDEargs):
-        inherit_docstrings_from_interface(self, "FixedBandwidthKDEBGInjector")
-
-        self.XtoY = XtoY
-        self.YtoX = YtoX
-        self.bandwidth = bandwidth
-        self.kernel = kernel
-        self.rtol = rtol
-
-        if XtoY is None or YtoX is None:
-            raise ValueError("Transformation functions can't be None.")
-
-        if kernel not in self._VALID_KERNELS:
-            raise ValueError("Invalid kernel: '{}'".fromat(kernel))
+    def __init__(self, glob_bw="silverman", alpha=0.5,
+                 diag_cov=False, max_gb=2.):
+        inherit_docstrings_from_interface(self, "KDEBGInjector")
 
         # Create KDE model
-        self.kde_model = skn.KernelDensity(bandwidth=bandwidth,
-                                           kernel=kernel,
-                                           rtol=rtol,
-                                           **KDEargs
-                                           )
+        self.kde_model = KDE.GaussianKDE(glob_bw=glob_bw, alpha=alpha,
+                                         diag_cov=diag_cov, max_gb=max_gb)
 
         return
 
-    def fit(self, X):
+    def fit(self, X, bounds=None):
         self._n_features = X.shape[1]
-
-        # Transform data X to KDE space Y
-        Y = self.XtoY(X)
-
-        self.kde_model.fit(Y)
+        self._bounds = self._check_bounds(bounds)
+        # TODO: Use real bounds via mirror method in KDE class
+        self.kde_model.fit(X)
         return
 
-    def sample(self, n_samples=1, range=None, random_state=None):
-        range, random_state = check_sample_params(self, n_samples, range,
-                                                  random_state)
-
-        # Scale ranges to KDE space
-        range = self.XtoY(range)
+    def sample(self, n_samples=1, random_state=None):
+        random_state = self._check_sample_params(n_samples, random_state)
+        rng = self._bounds
 
         # Check which samples are in range, redraw those that are not
-        Y = np.empty((0, self._n_features))
+        X = []
         while n_samples > 0:
             gen = self.kde_model.sample(n_samples, random_state)
-            accepted = np.all(np.logical_and(gen >= range[0],
-                                             gen <= range[1]), axis=1)
+            accepted = np.all(np.logical_and(gen >= rng[:, 0],
+                                             gen <= rng[:, 1]), axis=1)
             n_samples = np.sum(~accepted)
-            print("Not accepted : ", n_samples)
             # Append accepted to final sample
-            Y = np.append(Y, gen[accepted], axis=0)
+            X.append(gen[accepted])
 
-        # Unscale sample to get original feature space
-        return self.YtoX(Y)
+        return np.concatenate(X)
 
 
 class DataBGInjector(BGInjector):
@@ -160,71 +164,55 @@ class DataBGInjector(BGInjector):
         return
 
     def fit(self, X):
-        # The 'model' is the data itself
+        # The 'model' is simply the data itself
         self.X = np.copy(X)
         self._n_features = X.shape[1]
         return
 
-    def sample(self, n_samples=1, range=None, random_state=None):
-        range, random_state = check_sample_params(self, n_samples, range,
-                                                  random_state)
-
-        # Draw indices uniformly from the data in range and return the events
-        X = self.X
-        accepted = np.all(np.logical_and(X >= range[0],
-                                         X <= range[1]), axis=1)
-        X = X[accepted]
-        idx = random_state.randint(X.shape[0], size=n_samples)
-        return X[idx]
+    def sample(self, n_samples=1, random_state=None):
+        rndgen = self._check_sample_params(n_samples, random_state)
+        # Draw indices uniformly from the data
+        idx = rndgen.randint(self.X.shape[0], size=n_samples)
+        return self.X[idx]
 
 
 class UniformBGInjector(BGInjector):
     # "Close" to real data
     _logE_mean = 3.
-    _logE_sigma = 1.
-    _sigma_scale = 2.
+    _logE_sigma = .25
+    _sigma_scale = 3.
     """
     Uniform Background Injector
 
     Background Injector creating uniform events on the whole sky.
-    Created features are logE from a gaussian with given mean and covariance,
-    declination in radian uniformly distributed on the sphere and sigma in
-    radianfrom an exponential distribution with scale beta.
-
-    Note: Uses emcee to sample a nice sigma distribtuion x * exp(-x)
+    Created features are:
+        - logE from a gaussian with mean 3 and stddev .5
+        - Declination in rad uniformly distributed in sinDec
+        - Sigma in rad from 3^2 * x * exp(-3 * x)
     """
     def __init__(self):
         inherit_docstrings_from_interface(self, "UniformBGInjector")
         return
 
     def fit(self, X=None):
+        # Model is completely generated, only save n_features for consitency
         self._n_features = 3
         return
 
-    def sample(self, n_samples=1, range=None, random_state=None):
-        range, random_state = check_sample_params(self, n_samples, range,
-                                                  random_state)
+    def sample(self, n_samples=1, random_state=None):
+        random_state = self._check_sample_params(n_samples, random_state)
+        X = np.zeros((n_samples, self._n_features), dtype=np.float)
 
-        X = np.zeros((n_samples, 3), dtype=np.float)
-
+        # Sample logE from gaussian, sinDec uniform, sigma from x*exp(-x)
         X[:, 0] = random_state.normal(self._logE_mean,
                                       self._logE_sigma, size=n_samples)
-        X[:, 1] = np.arccos(random_state.uniform(-1, 1, size=n_samples))
 
-        # Sample a nice sigma with x * exp(-x)
-        def logpdf(x):
-            if x < 0:
-                return -np.inf
-            return np.log(x) - self._sigma_scale * x
+        X[:, 1] = (np.arccos(random_state.uniform(-1, 1, size=n_samples)) -
+                   np.pi / 2.)
 
-        import emcee
-        n_walkers = 6
-        n_mcmc = int(np.ceil(n_samples / n_walkers))
-        sampler = emcee.EnsembleSampler(dim=1, nwalkers=n_walkers,
-                                        lnpostfn=logpdf)
-        sampler.run_mcmc(N=n_mcmc,
-                         pos0=np.random.uniform(0.4, 0.6, size=(n_walkers, 1)))
-        X[:, 2] = np.deg2rad(sampler.flatchain.flatten()[:n_samples])
+        # From pythia8: home.thep.lu.se/~torbjorn/doxygen/Basics_8h_source.html
+        u1, u2 = np.random.uniform(size=(2, n_samples))
+        X[:, 2] = np.deg2rad(-np.log(u1 * u2) / self._sigma_scale)
 
         return X
 
@@ -232,36 +220,6 @@ class UniformBGInjector(BGInjector):
 # #############################################################################
 # ## Utils
 # #############################################################################
-
-
-def check_sample_params(self, n_samples, range, random_state):
-    """
-    Just performs checks on parameter sanity for the BGInjector sample method.
-
-    The `random_state` is passed to `sklearn.util.check_random_state` which
-    creates a new RandomState depending on the input type or just returns the
-    RandomState if given.
-
-    The range is multiplied with the scale to match the data scale.
-    """
-    if n_samples < 1:
-        raise ValueError("'n_samples' must be at least 1.")
-
-    if range is None:
-        range = np.repeat([[-np.inf, np.inf], ],
-                          repeats=self._n_features, axis=0).T
-    range = np.array(range)
-    if range.shape[0] != 2:
-        raise ValueError("Invalid 'range'. Must be shape (2, n_features).")
-    elif np.any(np.equal(range, None)):
-        raise ValueError("'None' not allowed, use `+-np.inf` instead.")
-
-    # Just selects depending on None, Int or RandomState type
-    random_state = check_random_state(random_state)
-
-    return range, random_state
-
-
 def inherit_docstrings_from_interface(self, descr, func=None):
     """
     Inherits all function docstrings from superclass functions defined in its
